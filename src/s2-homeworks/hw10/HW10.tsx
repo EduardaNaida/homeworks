b20e46715ea80a12e9147a910f449aa5a67ba8eb
EduardaNaida <eduarda.naida@gmail.com>
2022-11-26 17:39:26 +0300

    HW 10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -4,6 +4,7 @@ import {AppStoreType} from './bll/store'
 import {loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
+import styles from './Loader.module.css'
 import {Loader} from './Loader'
 
 /*
@@ -14,13 +15,17 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
+    const dispatch = useDispatch();
     // useSelector, useDispatch // пишет студент
-    const isLoading = false
+    const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
 
     const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
         // dispatch
-
+        dispatch(loadingAC(true))
         // setTimeout
+        setTimeout(() => {
+            dispatch(loadingAC(false))
+        }, 1500)
     }
 
     return (
@@ -35,6 +40,7 @@ const HW10 = () => {
                 ) : (
                     <SuperButton
                         id={'hw10-button-start-loading'}
+                        className={styles.button}
                         onClick={setLoading}
                     >
                         Set loading...
