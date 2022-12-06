dbf895e219bdfd972370f9685c6543eaae708056
EduardaNaida <eduarda.naida@gmail.com>
2022-12-06 17:35:44 +0300

    hw 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,29 @@
+import store from "../../hw10/bll/store";
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // дописать
 
+export const themeReducer = (state = initState, action: ChangeThemeType): typeof initState => { // fix any
+    switch (action.type) {
+
+        case "SET_THEME_ID": {
+            return {
+                ...state,
+                themeId: action.id
+            }
+        }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number) => ({type: 'SET_THEME_ID', id: id}) // fix any
+
+export type ChangeThemeType = {
+    type: 'SET_THEME_ID',
+    id: number
+}
+
+export type RootState = ReturnType<typeof store.getState>;
