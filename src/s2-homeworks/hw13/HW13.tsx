3d7dee72dbf7656ab8e9fe192f7ee6f9eb69810d
EduardaNaida <eduarda.naida@gmail.com>
2022-12-13 22:17:20 +0300

    upd

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -36,7 +36,8 @@ const HW13 = () => {
             .then((res) => {
                 setCode('Код 200!')
                 setImage(success200)
-                setText('...всё ок)')
+                setText('...всё ок)' +
+                    'код 200 - обычно означает что скорее всего всё ок)')
                 setInfo('')
                 // дописать
 
@@ -46,18 +47,22 @@ const HW13 = () => {
                 if (e.message === 'Request failed with status code 500') {
                     setCode('Код 500!')
                     setImage(error500)
-                    setText('эмитация ошибки на сервере')
+                    setText('эмитация ошибки на сервере' +
+                        'ошибка 500 - обычно означает что что-то сломалось на сервере, например база данных)')
                     setInfo('')
                 } else if (e.message === 'Request failed with status code 400') {
                     setImage(error400)
                     setCode('Код 400!')
-                    setText('Ты не отправил success в body вообще!')
+                    setText('Ты не отправил success в body вообще!' +
+                        'ошибка 400 - обычно означает что скорее всего фронт отправил что-то не то на бэк!')
                     setInfo('')
                 } else {
+                    console.log(e.message)
                     setImage(errorUnknown)
                     setInfo('')
                     setCode('Error!')
-                    setText('Error')
+                    setText('Network Error' +
+                        'AxiosError')
                 }
             })
     }
